04e74e38a57ae46f01a9aeeb5678395b6e65b07f
Minuk Song <musong@pubg.com>
2023-09-08 17:09:55 +0900

    fix: add --version flag and fix ci

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/lyft/protoc-gen-star/v2"
 	"github.com/pubg/protoc-gen-venus/pkg/modules"
 	"google.golang.org/protobuf/types/pluginpb"
 )
 
+var version string = "develop"
+
 func main() {
+	if len(os.Args) == 2 {
+		if os.Args[1] == "--version" {
+			fmt.Println(version)
+		} else if os.Args[1] == "--help" {
+			fmt.Println("USAGE:")
+			fmt.Println("  protoc-gen-debug --version  : print version")
+		}
+		return
+	}
+
 	feature := uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 
 	pgs.Init(
